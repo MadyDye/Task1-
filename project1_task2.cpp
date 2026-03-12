0ff504283692219b9364b758e8696ab2c409a646
MadyDye <97874936+MadyDye@users.noreply.github.com>
2026-03-12 09:19:50 -0500

    Add main function for fraction operations

---
+++ project1_task2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "newfraction1.h"
+using namespace std;
+
+int main() {
+
+    Fraction f1, f2;
+
+    int n,d;
+
+    cout << "Enter the first fraction: numerator denominator\n";
+    cin >> n >> d;
+    f1.setNumerator(n);
+    f1.setDenominator(d);
+
+    cout << "Enter the second fraction: numerator denominator\n";
+    cin >> n >> d;
+    f2.setNumerator(n);
+    f2.setDenominator(d);
+
+    cout << "f1 = ";
+    f1.display();
+    cout << endl;
+
+    cout << "f2 = ";
+    f2.display();
+    cout << endl;
+
+    cout << "f1 + f2 = ";
+    add(f1,f2).display();
+    cout << endl;
+
+    cout << "f1 - f2 = ";
+    subtract(f1,f2).display();
+    cout << endl;
+
+    cout << "f1 * f2 = ";
+    multiply(f1,f2).display();
+    cout << endl;
+
+    cout << "f1 / f2 = ";
+    divide(f1,f2).display();
+    cout << endl;
+
+    return 0;
+}
